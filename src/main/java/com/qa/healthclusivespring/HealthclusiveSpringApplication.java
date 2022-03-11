6b3fee8b40dc755babd5dc1a118c6a6b412e98e3
EmiliaWlo <emiliawlodarska@gmail.com>
2022-03-11 19:31:57 +0000

    finished domain class

--- src/main/java/com/qa/healthclusivespring/HealthclusiveSpringApplication.java
+++ src/main/java/com/qa/healthclusivespring/HealthclusiveSpringApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HealthclusiveSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HealthclusiveSpringApplication.class, args);
+		SpringApplication.run(HealthclusiveSpringApplication.class, args);	
 	}
 
 }
---
+++ src/main/java/com/qa/healthclusivespring/domain/Therapists.java
@@ -0,0 +1,5 @@
+package com.qa.healthclusivespring.domain;
+
+public class Therapists {
+
+}
