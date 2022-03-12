ca69cedc6e6a2b5b262db3c1e3eed5344f2c9d22
EmiliaWlo <emiliawlodarska@gmail.com>
2022-03-12 00:04:12 +0000

    Progress on Controller and Service

--- src/main/java/com/qa/healthclusivespring/rest/TherapistsController.java
+++ src/main/java/com/qa/healthclusivespring/rest/TherapistsController.java
@@ -1,22 +1,29 @@
 package com.qa.healthclusivespring.rest;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.qa.healthclusivespring.domain.Therapists;
 import com.qa.healthclusivespring.service.TherapistsService;
 
 @RestController
 public class TherapistsController {
-	
 	private TherapistsService service;
-	
-	
-	
-	
+
 	public TherapistsController(TherapistsService service) {
 		super();
 		this.service = service;
+		
+	}
+		
+	//create (post request)
+	@PostMapping("/create")
+	public Therapists createMap(@RequestBody Therapists t) {
+		return this.service.create(t);
+		
+		
 	}
 	
-	
-	
+		
 }
--- src/main/java/com/qa/healthclusivespring/service/TherapistsService.java
+++ src/main/java/com/qa/healthclusivespring/service/TherapistsService.java
@@ -1,5 +1,25 @@
 package com.qa.healthclusivespring.service;
 
+import org.springframework.stereotype.Service;
+
+import com.qa.healthclusivespring.domain.Therapists;
+import com.qa.healthclusivespring.repo.TherapistsRepo;
+
+@Service
 public class TherapistsService {
+	
+	private TherapistsRepo repo;
+
+	public TherapistsService(TherapistsRepo repo) {
+		super();
+		this.repo = repo;
+	}
+	
+	//create-logic
+	
+	public Therapists create(Therapists newTherapist) {
+		return this.repo.save(newTherapist);
+		
+	}
 
 }
